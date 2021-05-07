0f149209cd8fa9a5a9cca34b7fae53b894e5419e
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-07 16:16:42 +0600

    add new code in semaphore 2.0

--- BSSE1129.c
+++ BSSE1129.c
@@ -2,59 +2,60 @@
 #include<pthread.h>
 #include<semaphore.h>
 
-sem_t mutex,writeblock;
+sem_t reader_process,writer_process;
 int data = 0,rcount = 0;
 
 void *reader(void *arg)
 {
   int f;
   f = ((int)arg);
-  sem_wait(&mutex);
+  sem_wait(&reader_process);
   rcount++;
+  
   if(rcount==1){
-    sem_wait(&writeblock);
+    sem_wait(&writer_process);
   }
 
-  sem_post(&mutex);
-  printf("Data read by the reader%d is %d\n",f,data);
+  sem_post(&reader_process);
+  printf("Data read by the reader(%d) is %d\n",f,data);
 
-  sem_wait(&mutex);
+  sem_wait(&reader_process);
   rcount--;
   if(rcount==0)
   {
-      sem_post(&writeblock);
+      sem_post(&writer_process);
 
   }
 
-  sem_post(&mutex);
+  sem_post(&reader_process);
 }
 
 void *writer(void *arg)
 {
   int f;
   f = ((int) arg);
-  sem_wait(&writeblock);
+  sem_wait(&writer_process);
   data++;
-  printf("Data writen by the writer%d is %d\n",f,data);
-  sleep(1);
-  sem_post(&writeblock);
+  printf("Data written by the writer(%d)is %d\n",f,data);
+  
+  sem_post(&writer_process);
 }
 
 int main()
 {
   int i,b;
-  pthread_t rtid[10],wtid[10];
-  sem_init(&mutex,0,1);
-  sem_init(&writeblock,0,1);
+  pthread_t reader_t_id[10],writer_t_id[10];
+  sem_init(&reader_process,0,1);
+  sem_init(&writer_process,0,1);
   for(i=0;i<10;i++)
   {
-    pthread_create(&wtid[i],NULL,writer,(void *)i);
-    pthread_create(&rtid[i],NULL,reader,(void *)i);
+    pthread_create(&writer_t_id[i],NULL,writer,(void *)i);
+    pthread_create(&reader_t_id[i],NULL,reader,(void *)i);
   }
   for(i=0;i<10;i++)
   {
-    pthread_join(wtid[i],NULL);
-    pthread_join(rtid[i],NULL);
+    pthread_join(writer_t_id[i],NULL);
+    pthread_join(reader_t_id[i],NULL);
   }
   return 0;
 }
