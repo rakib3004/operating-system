8321db0074d37fb9e99187ada7f0aeffbea1155c
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-07 16:07:10 +0600

    add new code in semaphore

--- BSSE1129.c
+++ BSSE1129.c
@@ -17,7 +17,7 @@ void *reader(void *arg)
 
   sem_post(&mutex);
   printf("Data read by the reader%d is %d\n",f,data);
-  sleep(1);
+
   sem_wait(&mutex);
   rcount--;
   if(rcount==0)
@@ -43,15 +43,15 @@ void *writer(void *arg)
 int main()
 {
   int i,b;
-  pthread_t rtid[5],wtid[5];
+  pthread_t rtid[10],wtid[10];
   sem_init(&mutex,0,1);
   sem_init(&writeblock,0,1);
-  for(i=0;i<=2;i++)
+  for(i=0;i<10;i++)
   {
     pthread_create(&wtid[i],NULL,writer,(void *)i);
     pthread_create(&rtid[i],NULL,reader,(void *)i);
   }
-  for(i=0;i<=2;i++)
+  for(i=0;i<10;i++)
   {
     pthread_join(wtid[i],NULL);
     pthread_join(rtid[i],NULL);
