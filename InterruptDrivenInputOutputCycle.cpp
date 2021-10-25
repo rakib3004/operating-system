a75af876d58332126690531dffd12dda860fb154
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-25 12:37:16 +0600

    complete template of I/O Controller

--- InterruptDrivenInputOutputCycle.cpp
+++ InterruptDrivenInputOutputCycle.cpp
@@ -1,50 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-void CPU(){
-
-printf("---------------------------------\n");
-printf("============ CPU ================\n");
-printf("----------------------------------\n");
-time_t now = time(0);
-
-   char* dt = ctime(&now);
-
-   cout << "*_*_*_*_*_*_*_" << dt << "*_*_*_*_*_*_*_"  << endl;
-
-cout<<"Device Driver Initiates Input Output at CPU"<<endl;
-
-
-cout<<"Go to I/O Controller  -----> "<<endl;
-
-InputOutputController();
-
-cout<<"I/O Controller's action is finished !!!!!!  -----> "<<endl;
-
-
-return;
-
-}
-
-
+void CPU();
 
 
 
 // Input Output Controller Section !!!!!!!!!!!!
-
-void InputOutputController(){
-
-printf("---------------------------------\n");
-printf("======== I/O Controller ==========\n");
-printf("----------------------------------\n");
-
-initiateIO();
-getInterruptSignals();
-
-return;
-
-}
 
 void initiateIO(){
 
@@ -62,9 +22,98 @@ cout<<"3.Error Signal (which is generate)\n";
 cout<<"---------------------------"<<endl;
 
 
+}
+
+void InputOutputController(){
+
+printf("---------------------------------\n");
+printf("======== I/O Controller ==========\n");
+printf("----------------------------------\n");
+
+initiateIO();
+getInterruptSignals();
+
+return;
+
+}
+
+
+
+
+
+
+
+void initiateDeviceDriverIO(){
+cout<<"Device Driver Initiates Input Output at CPU"<<endl;
+cout<<"Go to I/O Controller  -----> "<<endl;
+
+InputOutputController();
+
+cout<<"I/O Controller's action is finished !!!!!!  -----> "<<endl;
+return;
+
+}
+
+
+void receivingInterrupt(){
+
+cout<<"CPU receive all interrupt signals"<<endl<<endl;
+cout<<"I/O Controller ->O  ->O ->O ->O ->O CPU"<<endl<<endl;
+cout<<"Transfer Control to Interrupt Handler"<<endl<<endl;
+cout<<"CPU Controller ->O  ->O ->O ->O ->O Interrupt Handler"<<endl<<endl;
 
 
 }
+
+
+void processData(){
+
+
+cout<<"Interrupt Handler Process Data!!"<<endl;
+
+cout<<"->O ->O ===> -># -># -># "<<endl;
+
+cout<<"Return to I/O Controller (Interrupts)"<<endl;
+
+cout<<"Interrupt Handler -># -># -># -># -># I/O Controller (Interrupts) "<<endl;
+
+}
+
+void resumesCpuInterruptedTask(){
+
+
+cout<<"Restart CPU's remaining and new interrupt task"<<endl;
+CPU();
+
+}
+
+
+
+
+void CPU(){
+
+printf("---------------------------------\n");
+printf("============ CPU ================\n");
+printf("----------------------------------\n");
+time_t now = time(0);
+
+   char* dt = ctime(&now);
+
+   cout << "*_*_*_*_*_*_*_" << dt << "*_*_*_*_*_*_*_"  << endl;
+
+initiateDeviceDriverIO();
+receivingInterrupt();
+
+ processData();
+
+ resumesCpuInterruptedTask();
+
+
+
+return;
+
+}
+
 
 
 
