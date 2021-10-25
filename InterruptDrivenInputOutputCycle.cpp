eb9a75465716f83e5c5b015825a2ee1fa4223b41
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-25 12:21:41 +0600

    adding CPU, Interrupt Handler and Initiate I/O and I/O device driver

--- InterruptDrivenInputOutputCycle.cpp
+++ InterruptDrivenInputOutputCycle.cpp
@@ -18,13 +18,20 @@ cout<<"Device Driver Initiates Input Output at CPU"<<endl;
 
 cout<<"Go to I/O Controller  -----> "<<endl;
 
+InputOutputController();
 
-
+cout<<"I/O Controller's action is finished !!!!!!  -----> "<<endl;
 
 
 return;
 
 }
+
+
+
+
+
+// Input Output Controller Section !!!!!!!!!!!!
 
 void InputOutputController(){
 
@@ -32,10 +39,34 @@ printf("---------------------------------\n");
 printf("======== I/O Controller ==========\n");
 printf("----------------------------------\n");
 
+initiateIO();
+getInterruptSignals();
 
 return;
 
 }
+
+void initiateIO(){
+
+cout<<"Initiate I/O"<<endl;
+
+}
+
+void getInterruptSignals(){
+
+cout<<"---------------------------"<<endl;
+cout<<"   3 Basic Interrupt Signals: \n\n";
+cout<<"1.Input Signal (which is ready)\n";
+cout<<"2.Output Signal (which is complete)\n";
+cout<<"3.Error Signal (which is generate)\n";
+cout<<"---------------------------"<<endl;
+
+
+
+
+}
+
+
 
 
 int main()
