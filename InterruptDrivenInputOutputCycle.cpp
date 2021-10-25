58a5835b6039c87f9e15cedf8bfe8d8667e0aa9c
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-25 11:56:57 +0600

    initiate I/O Interrupt

---
+++ InterruptDrivenInputOutputCycle.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void CPU(){
+
+printf("---------------------------------\n");
+printf("============ CPU ================\n");
+printf("----------------------------------\n");
+time_t now = time(0);
+
+   char* dt = ctime(&now);
+
+   cout << "*_*_*_*_*_*_*_" << dt << "*_*_*_*_*_*_*_"  << endl;
+
+cout<<"Device Driver Initiates Input Output at CPU"<<endl;
+
+
+cout<<"Go to I/O Controller  -----> "<<endl;
+
+
+
+
+
+return;
+
+}
+
+void InputOutputController(){
+
+printf("---------------------------------\n");
+printf("======== I/O Controller ==========\n");
+printf("----------------------------------\n");
+
+
+return;
+
+}
+
+
+int main()
+{
+
+
+printf("---- Welcome to the Interrupt I/O Cycle -----\n");
+printf("\n");
+printf("\n");
+
+int i;
+
+
+for(i=0;i<1;i++){
+  CPU();
+}
+
+
+
+
+}
