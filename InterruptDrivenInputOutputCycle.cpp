8d5724983c7d25a34a77e2d8713c20d5244ff887
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-27 14:28:07 +0600

    add sleep in each modeule

--- InterruptDrivenInputOutputCycle.cpp
+++ InterruptDrivenInputOutputCycle.cpp
@@ -4,21 +4,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#define  totalThreadNumbers 20
+#define  totalInterruptionNumbers 20
 using namespace std;
 void CPU();
+
+struct interruption{
+int interruptionState;
+void (*interruptionHandler)();
+
+};
+
+struct interruption interruptionVectorObject[totalInterruptionNumbers];
 
 
 
 void interruptionVectorStates()
 {
 
-    int totalThreadNumbers = 20;
-
     pthread_t threads[totalThreadNumbers];
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     time_t t;
     srand(time(&t));
+
 
 
 }
@@ -29,7 +38,7 @@ void initiateIO()
 {
     cout<<"---------------------------"<<endl;
     cout<<"1.1 Initiate I/O"<<endl;
-    cout<<"---------------------------"<<endl<<endl<<endl;
+    cout<<"---------------------------"<<endl<<endl<<endl;     sleep(2);
 
 }
 
@@ -43,11 +52,11 @@ void getInterruptSignals()
     cout<<"ii.Output Signal (which is complete)\n";
     cout<<"iii.Error Signal (which is generate)\n";
 
-    cout<<"Visualize Interruption ==>"<<endl<<endl;
+    cout<<"Visualize Interruption ==>"<<endl<<endl;     sleep(2);
     interruptionVectorStates();
 
     cout<<"----------------------------------------"<<endl;
-    cout<<"----------------------------------------"<<endl<<endl<<endl;
+    cout<<"----------------------------------------"<<endl<<endl<<endl;     sleep(2);
 
 
 }
@@ -72,17 +81,17 @@ void initiateDeviceDriverIO()
 {
     cout<<"----------------------------------------------"<<endl;
     cout<<"1. Device Driver Initiates Input Output at CPU"<<endl;
-    cout<<"-----------------------------------------------"<<endl<<endl<<endl;
+    cout<<"-----------------------------------------------"<<endl<<endl<<endl;     sleep(2);
 
     cout<<"---------------------------------"<<endl;
     cout<<"1.a. Go to I/O Controller  -----> "<<endl;
-    cout<<"---------------------------------"<<endl<<endl<<endl;
+    cout<<"---------------------------------"<<endl<<endl<<endl;     sleep(2);
 
 
     InputOutputController();
     cout<<"--------------------------------------------------------"<<endl;
     cout<<"1.b. I/O Controller's action is finished !!!!!!  -----> "<<endl;
-    cout<<"======================================================="<<endl<<endl<<endl;
+    cout<<"======================================================="<<endl<<endl<<endl;     sleep(2);
 
     return;
 
@@ -95,13 +104,13 @@ void receivingInterrupt()
     cout<<"2.CPU receive all interrupt signals"<<endl;
     cout<<"I/O Controller ->O  ->O ->O ->O ->O CPU"<<endl;
     cout<<"[O means data steam]"<<endl;
-    cout<<"-----------------------------------"<<endl<<endl<<endl;
+    cout<<"-----------------------------------"<<endl<<endl<<endl;     sleep(2);
 
     cout<<"-----------------------------------"<<endl;
     cout<<"2.a. Transfer Control to Interrupt Handler"<<endl;
     cout<<"CPU Controller ->O  ->O ->O ->O ->O Interrupt Handler"<<endl;
     cout<<"[O means data steam]"<<endl;
-    cout<<"-----------------------------------"<<endl<<endl<<endl;
+    cout<<"-----------------------------------"<<endl<<endl<<endl;     sleep(2);
 
 
 
@@ -115,14 +124,14 @@ void processData()
     cout<<"3. Interrupt Handler Process Data!!"<<endl;
     cout<<"->O ->O ===> -># -># -># "<<endl;
     cout<<"[# means processed data steam]"<<endl;
-    cout<<"-----------------------------------"<<endl<<endl<<endl;
+    cout<<"-----------------------------------"<<endl<<endl<<endl;     sleep(2);
 
 
     cout<<"-----------------------------------"<<endl;
     cout<<"3.a. Return to I/O Controller (Interrupts)"<<endl;
     cout<<"Interrupt Handler -># -># -># -># -># I/O Controller (Interrupts) "<<endl;
     cout<<"[# means processed data steam]"<<endl;
-    cout<<"-----------------------------------"<<endl<<endl<<endl;
+    cout<<"-----------------------------------"<<endl<<endl<<endl;     sleep(2);
 
 
 }
@@ -132,7 +141,7 @@ void resumesCpuInterruptedTask()
 
     cout<<"-----------------------------------"<<endl;
     cout<<"4.Restart CPU's remaining and new interrupt task"<<endl;
-    cout<<"-----------------------------------"<<endl<<endl<<endl;
+    cout<<"-----------------------------------"<<endl<<endl<<endl;     sleep(2);
 
 }
 
