6b320dab075c73b81503792a4db12c311a608c6b
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-11-16 15:48:48 +0600

    creating multithread programming

--- UnixForkCreatingProcess.c
+++ UnixForkCreatingProcess.c
@@ -4,22 +4,27 @@
 
 int main(){
 
-pid_t pid;
-printf("Mushfiq said Aynabahini");
+pid_t pid, pid2, pid3, pid4;
 
-
+printf("Process 1 ID is: %d \n",pid);
 pid = fork();
+pid2 = fork();
+pid3 = fork();
+pid4 = fork();
 
-printf("The ID is: %d ",pid);
+
+printf("Process 2 ID is: %d \n",pid);
 
 if(pid<0){
-fprintf(stderr, "Fork Failed");
+fprintf(stderr, "Fork Failed\n");
 return 1;
 
 }
 
 else if(pid ==0){
 //execlp("/bin/ls", "ls", NULL);
+printf("Process 3 ID is: %d \n",pid);
+
 
 }
 
@@ -27,7 +32,7 @@ else{
 
 wait(NULL);
 
-printf("Child Complete");
+printf("Process 4 ID is: %d \n",pid);
 
 
 }
